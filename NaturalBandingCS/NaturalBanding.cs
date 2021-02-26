61d07168c296d601ec35e15e83e1615ead1a075a
Joshua Patterson <jleegippies@gmail.com>
2021-02-26 16:02:58 -0600

    WIP Added GetSdcmAll method

--- NaturalBandingCS/NaturalBanding.cs
+++ NaturalBandingCS/NaturalBanding.cs
@@ -4,12 +4,18 @@ using System.Linq;
 
 namespace NaturalBandingCS {
     public class NaturalBanding {
-        public static List<double> Jenks(List<double> inputList, int numOfBands) {
+
+        private static double GetSdcmAll(List<List<double>> inputListList) {
+            var sdcmAll = (from iList in inputListList let mean = iList.Sum() / iList.Count select iList.Sum(value => Math.Pow(value - mean, 2))).Sum();
+            return sdcmAll;
+        }
+        
+        public static List<Tuple<double, double>> Jenks(List<double> inputList, int numOfBands) {
             inputList.Sort();
             var mean = inputList.Sum() / inputList.Count;
             var sdam = inputList.Sum(input => Math.Pow(input - mean, 2.0));
 
-            var sdcmAllList = new List<double>();
+            var sdcmAllList = new List<Tuple<double, double>>();
             for (var i = 1; i < inputList.Count; i++) {
                 var firstSum = 0.0;
                 for (var j = 0; j < i; j++) {
@@ -32,17 +38,15 @@ namespace NaturalBandingCS {
                 for (var j = i; j < inputList.Count; j++) {
                     sdcmAll += Math.Pow(inputList[j] - secondMean, 2);
                 }
-                sdcmAllList.Add(sdcmAll);
+                sdcmAllList.Add(new Tuple<double, double>(inputList[i], sdcmAll));
             }
 
-            var gvfList = sdcmAllList.Select(sdcm => (sdam - sdcm) / sdam).ToList();
-            return gvfList;
+            return sdcmAllList.Select(t => new Tuple<double, double>(t.Item1, (sdam - t.Item2) / sdam)).ToList();
         }
 
         public static void Main() {
-            var inputList = new List<double> {4, 5, 9, 10};
-            Jenks(inputList, 2).ForEach(Console.WriteLine);
-            Console.WriteLine("Hello World");
+            var inputList = new List<double> {12, 13, 14, 15, 16, 17, 18, 19, 20, 21};
+            Jenks(inputList, 5).ForEach(Console.WriteLine);
         }
     }
 }
