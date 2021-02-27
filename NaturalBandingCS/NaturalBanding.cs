b6c951083dc6838f3bfa4df85131a4f8d8a61dab
Joshua Patterson <jleegippies@gmail.com>
2021-02-27 16:36:11 -0600

    WIP Got it working for one band!

--- NaturalBandingCS/NaturalBanding.cs
+++ NaturalBandingCS/NaturalBanding.cs
@@ -5,18 +5,23 @@ using System.Linq;
 namespace NaturalBandingCS {
     public class NaturalBanding {
 
-        public static List<Tuple<double, double>> Jenks(List<double> inputList, int numOfBands) {
+        public static List<Tuple<List<List<double>>, double>> Jenks(List<double> inputList, int numOfBands) {
             inputList.Sort();
             var mean = inputList.Sum() / inputList.Count;
             var sdam = inputList.Sum(input => Math.Pow(input - mean, 2.0));
 
-            var sdcmAllList = new List<Tuple<double, double>>();
+            var sdcmAllList = new List<Tuple<List<List<double>>, double>>();
             var bandIndexList = GetInitialBandIndexList(inputList.Count, numOfBands);
-            var dividedList = GetDividedList(inputList, bandIndexList);
-            var sdcmAll = GetSdcmAll(dividedList);
-            sdcmAllList.Add(new Tuple<double, double>(1, sdcmAll));
+            var hasIncremented = true;
 
-            return sdcmAllList.Select(t => new Tuple<double, double>(t.Item1, (sdam - t.Item2) / sdam)).ToList();
+            while (hasIncremented) {
+                var dividedList = GetDividedList(inputList, bandIndexList);
+                var sdcmAll = GetSdcmAll(dividedList);
+                sdcmAllList.Add(new Tuple<List<List<double>>, double>(dividedList, sdcmAll));
+                hasIncremented = IncrementBandIndexList(bandIndexList);
+            }
+            
+            return sdcmAllList.Select(t => new Tuple<List<List<double>>, double>(t.Item1, (sdam - t.Item2) / sdam)).ToList();
         }
 
         private static List<int> GetInitialBandIndexList(int inputCount, int numOfBands) {
@@ -26,6 +31,14 @@ namespace NaturalBandingCS {
             }
             resultList.Add(inputCount);
             return resultList;
+        }
+
+        private static bool IncrementBandIndexList(List<int> bandIndexList) {
+            if (bandIndexList[bandIndexList.Count - 2] + 1 < bandIndexList[bandIndexList.Count - 1]) {
+                bandIndexList[bandIndexList.Count - 2]++;
+                return true;
+            }
+            return false;
         }
 
         private static List<List<double>> GetDividedList(List<double> inputList, List<int> bandIndexList) {
