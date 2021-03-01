580c86fe8aa37b108db0f39b883d54f5f7235a39
Joshua Patterson <jleegippies@gmail.com>
2021-02-28 18:09:42 -0600

    Lint Fixes

--- NaturalBandingCS/NaturalBanding2.cs
+++ NaturalBandingCS/NaturalBanding2.cs
@@ -3,20 +3,17 @@ using System.Collections.Generic;
 
 namespace NaturalBandingCS {
     public class NaturalBanding2 {
-        public static List<double> JenksBreakValues(List<double> values, int nb_class) {
-            int i3, i4;
-            int i = 0, j = 0, l = 0, m = 0, k = nb_class;
-            double v, val, s1, s2, w;
-            double[] kclass;
+        public static List<double> JenksBreakValues(List<double> values, int nbClass) {
+            int i, j, l, k = nbClass;
             var mat1 = new List<List<double>>();
             var mat2 = new List<List<double>>();
-            List<double> row = null;
+            List<double> row;
             var breaks = new List<double>();
-            var length_array = values.Count;
+            var lengthArray = values.Count;
 
             values.Sort();
 
-            for (i = 0; i < length_array; i++) {
+            for (i = 0; i < lengthArray; i++) {
                 row = new List<double>();
                 for (j = 0; j < k; j++) {
                     row.Add(1.0);
@@ -24,7 +21,7 @@ namespace NaturalBandingCS {
                 mat1.Add(row);
             }
 
-            for (i = 0; i < length_array; i++) {
+            for (i = 0; i < lengthArray; i++) {
                 row = new List<double>();
                 for (j = 0; j < k; j++) {
                     row.Add(Double.MaxValue);
@@ -32,17 +29,19 @@ namespace NaturalBandingCS {
                 mat2.Add(row);
             }
 
-            v = 0;
-            for (l = 2; l <= length_array; l++) {
-                s1 = s2 = w = 0;
-                for (m = 1; m <= l; m++) {
-                    i3 = l - m + 1;
-                    val = values[i3 - 1];
-                    s2 += val * val;
+            double v = 0;
+            for (l = 2; l <= lengthArray; l++) {
+                var s1 = 0.0;
+                var s2 = 0.0;
+                var w = 0.0;
+                for (var m = 1; m <= l; m++) {
+                    var i3 = l - m + 1;
+                    var val = values[i3 - 1];
+                    s2 += Math.Pow(val, 2.0);
                     s1 += val;
                     w++;
-                    v = s2 - (s1 * s1) / w;
-                    i4 = i3 - 1;
+                    v = s2 - Math.Pow(s1, 2.0) / w;
+                    var i4 = i3 - 1;
 
                     if (i4 != 0) {
                         for (j = 2; j <= k; j++) {
@@ -58,17 +57,17 @@ namespace NaturalBandingCS {
                 mat2[l - 1][0] = v;
             }
 
-            kclass = new double[k];
-            k = length_array;
-            for (j = nb_class; j > 1; j--) {
+            var kclass = new double[k];
+            k = lengthArray;
+            for (j = nbClass; j > 1; j--) {
                 kclass[j - 2] = k = (int) mat1[k - 1][j - 1] - 1;
             }
 
             breaks.Add(values[0]);
-            for (i = 1; i < nb_class; i++) {
+            for (i = 1; i < nbClass; i++) {
                 breaks.Add(values[(int) kclass[i - 1] - 1]);
             }
-            breaks.Add(values[length_array - 1]);
+            breaks.Add(values[lengthArray - 1]);
             return breaks;
         }
     }
